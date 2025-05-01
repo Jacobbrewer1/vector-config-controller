5d0a1605574a5338d805c0a1dea11b0f8ec9ed78
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-05-01 11:46:58 +0100

    updating the web version

--- vendor/github.com/jacobbrewer1/web/k8s/resource.go
+++ vendor/github.com/jacobbrewer1/web/k8s/resource.go
@@ -11,42 +11,62 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
-// UpsertableResource defines a resource which can be upserted.
+// UpsertableResource defines an interface for Kubernetes resources that can be upserted.
 type UpsertableResource interface {
+	// GetName retrieves the name of the resource.
 	GetName() string
+
+	// GetUID retrieves the unique identifier (UID) of the resource.
 	GetUID() types.UID
+
+	// SetUID sets the unique identifier (UID) of the resource.
 	SetUID(types.UID)
+
+	// GetResourceVersion retrieves the resource version, which is used
+	// for optimistic concurrency control in Kubernetes.
 	GetResourceVersion() string
+
+	// SetResourceVersion sets the resource version of the resource.
 	SetResourceVersion(string)
 }
 
-// Upserter defines an interface which should be implemented by something which wants to create or update Kubernetes resources.
+// Upserter defines a generic interface for managing Kubernetes resources.
 type Upserter[T UpsertableResource] interface {
+	// Create creates a new resource in the Kubernetes cluster.
 	Create(ctx context.Context, obj T, opts metav1.CreateOptions) (T, error)
+
+	// Update updates an existing resource in the Kubernetes cluster.
 	Update(ctx context.Context, obj T, opts metav1.UpdateOptions) (T, error)
+
+	// Get retrieves a resource from the Kubernetes cluster by its name.
 	Get(ctx context.Context, name string, opts metav1.GetOptions) (T, error)
 }
 
+// UpsertResource attempts to create or update a Kubernetes resource.
 func UpsertResource(ctx context.Context, kubeClient kubernetes.Interface, resource metav1.Object) error {
 	switch v := resource.(type) {
 	case *corev1.ConfigMap:
 		return upsert(ctx, kubeClient.CoreV1().ConfigMaps(resource.GetNamespace()), v)
+	case *corev1.Secret:
+		return upsert(ctx, kubeClient.CoreV1().Secrets(resource.GetNamespace()), v)
 	default:
 		return fmt.Errorf("unsupported resource type: %T", resource)
 	}
 }
 
+// upsert performs an upsert operation (create or update) for a Kubernetes resource.
 func upsert[U Upserter[T], T UpsertableResource](ctx context.Context, upserter U, obj T) error {
 	_, err := upserter.Get(ctx, obj.GetName(), metav1.GetOptions{})
 	if err != nil {
-		if k8serrors.IsNotFound(err) {
-			_, err := upserter.Create(ctx, obj, metav1.CreateOptions{})
-			if err != nil {
-				return fmt.Errorf("failed to create %T: %w", obj, err)
-			}
-			return nil
+		if !k8serrors.IsNotFound(err) {
+			return fmt.Errorf("failed to get %T: %w", obj, err)
 		}
-		return fmt.Errorf("failed to get %T: %w", obj, err)
+
+		obj.SetResourceVersion("") // Reset resource version for creation
+		if _, err := upserter.Create(ctx, obj, metav1.CreateOptions{}); err != nil {
+			return fmt.Errorf("failed to create %T: %w", obj, err)
+		}
+		return nil
 	}
 
 	obj, err = upserter.Update(ctx, obj, metav1.UpdateOptions{})
